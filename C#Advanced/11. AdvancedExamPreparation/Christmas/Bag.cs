be2eb21a6da16a19289799fbd3cca3c3a6929a73
dimitrinalu <lyubchova.dimitrina@gmail.com>
2020-02-21 12:32:35 +0200

    AddChanges

--- C#Advanced/11. AdvancedExamPreparation/Christmas/Bag.cs
+++ C#Advanced/11. AdvancedExamPreparation/Christmas/Bag.cs
@@ -6,42 +6,58 @@
 
     public class Bag
     {
-        private readonly List<Present> data;
+        private  List<Present> data;               
 
-        public Bag()
+        public Bag(string color, int capacity)            
         {
             this.data = new List<Present>();
-        }
-
-        public Bag(string color, int capacity)
-            : this()
-        {            
             this.Color = color;
             this.Capacity = capacity;
         }
 
-        public string Color { get; set; }
-        public int Capacity { get; set; }
         public int Count => this.data.Count;
+        public string Color { get; set; }
+        public int Capacity { get; set; }       
 
         public void Add(Present present)
         {
-            if (!(this.data.Count >= this.Capacity))
+            if (this.Capacity != 0 && !data.Contains(present))
             {
-                this.data.Add(present);
+                data.Add(present);
             }
         }
 
         public bool Remove(string name)
         {
-            return this.data
-                    .Remove(this.data
-                    .Where(p => p.Name == name)
-                    .FirstOrDefault());
+            Present present = this.data
+                    .FirstOrDefault(present => present.Name == name);
+
+            if (present != null)
+            {
+                this.data.Remove(present);
+
+                return true;
+            }
+
+            return false;           
         }
 
         public Present GetHeaviestPresent()
-            => this.data.OrderBy(w => w.Weight).First();
+        {
+            int maxWeight = int.MinValue;
+
+            Present heaviestPresent = null;
+
+            foreach (var present in data)
+            {
+                if (present.Weight > maxWeight)
+                {
+                    heaviestPresent = present;
+                }
+            }
+
+            return heaviestPresent;
+        }  
 
         public Present GetPresent(string name)
             => this.data.First(p => p.Name == name);
@@ -52,7 +68,7 @@
            
             sb.AppendLine($"{this.Color} bag contains:");
 
-            foreach (var present in this.data.Distinct())
+            foreach (var present in this.data)
             {
                 sb.AppendLine(present.ToString());
             }
