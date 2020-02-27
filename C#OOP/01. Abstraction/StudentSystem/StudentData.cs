398fb935b14fee7132556d973bd67199263bc6f1
dimitrinalu <lyubchova.dimitrina@gmail.com>
2020-02-27 15:13:40 +0200

    AddChanges

---
+++ C#OOP/01. Abstraction/StudentSystem/StudentData.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentSystem
+{    
+    using System.Collections.Generic;    
+
+    public class StudentData
+    {
+        public Dictionary<string, Student> Students { get; } = new Dictionary<string, Student>();
+
+        public void AddStudent(string name, int age, double grade)
+        {
+            if (!this.Students.ContainsKey(name))
+            {
+                var student = new Student(name, age, grade);
+                this.Students[name] = student;
+            }
+        }
+
+        public string GetStudentDetails(string name)
+        {
+            if (!this.Students.ContainsKey(name))
+            {
+                return null;
+            }
+
+            var student = this.Students[name];
+
+            return student.ToString();            
+        }       
+    }
+}
