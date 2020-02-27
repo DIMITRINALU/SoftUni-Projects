08101a7056feed28ea1e787da00ebe94ff6ce978
dimitrinalu <lyubchova.dimitrina@gmail.com>
2020-02-27 15:04:30 +0200

    AddFolder

---
+++ C#OOP/01. Abstraction/StudentSystem/ConsoleInputOutputProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentSystem
+{
+    using System;
+
+    public class ConsoleInputOutputProvider : IInputOutputProvider
+    {
+        public string GetInput() => Console.ReadLine();
+
+        public void ShowOutput(string data) => Console.WriteLine(data);
+    }
+}
---
+++ C#OOP/01. Abstraction/StudentSystem/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentSystem
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var studentData = new StudentData();
+            var inputOutputPrvider = new ConsoleInputOutputProvider(); 
+
+            var engine = new Engine(studentData, inputOutputPrvider);
+
+            engine.Process();
+        }
+    }
+}
