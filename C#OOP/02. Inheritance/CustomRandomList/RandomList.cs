c1e968f9dbeee9e5f0ed5cc2a2db1f48e95ceee1
dimitrinalu <lyubchova.dimitrina@gmail.com>
2020-03-02 12:53:58 +0200

    AddTask

---
+++ C#OOP/02. Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomRandomList 
+{
+    using System.Collections.Generic;
+
+    public class RandomList : List<string>
+    {  
+        public RandomList(List<string> list)
+        {
+            this.StringList = list;
+        }
+
+        public List<string> StringList { get; set; }
+
+        public string RandomString()
+        {
+            var random = new System.Random();
+
+            int index = random.Next(this.StringList.Count);
+            string randomString = this.StringList[index];
+
+            this.StringList.RemoveAt(index);
+
+            return randomString;
+        }
+    }
+}
