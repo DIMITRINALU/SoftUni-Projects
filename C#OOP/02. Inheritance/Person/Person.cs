1877049932ba7cc4b827b15a783e01391579a8c1
dimitrinalu <lyubchova.dimitrina@gmail.com>
2020-03-02 14:15:19 +0200

    AddTask

---
+++ C#OOP/02. Inheritance/Person/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace Person
+{
+    using System;
+    using System.Text;
+
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get => this.age;
+            set
+            {
+                if (value >= 0)
+                {
+                    this.age = value;
+                }                
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Name: {this.Name}, Age: {this.Age}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
