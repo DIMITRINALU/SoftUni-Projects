dbb1e1acb60b33a4c0cbb0c43294832b25070939
dimitrinalu <lyubchova.dimitrina@gmail.com>
2020-03-08 19:02:34 +0200

    AddChanges

--- C#Advanced/10. BasicAlgorithms/P02.RecursiveFactorial/Program.cs
+++ C#Advanced/10. BasicAlgorithms/P02.RecursiveFactorial/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
-
-namespace P02.RecursiveFactorial
+﻿namespace P02.RecursiveFactorial
 {
+    using System;
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int num = int.Parse(Console.ReadLine());
+            long result = Factorial(num);
+
+            Console.WriteLine(result);
+        }
+
+        private static long Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            return num * Factorial(num - 1);
         }
     }
 }
