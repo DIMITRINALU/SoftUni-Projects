8e65164b716c47662ed24d13674ab21c23a5d83b
dimitrinalu <lyubchova.dimitrina@gmail.com>
2020-03-08 14:16:08 +0200

    AddTask

---
+++ C#OOP/03. Encapsulation/Validation/Person.cs
@@ -0,0 +1,93 @@
+﻿namespace PersonsInfo    
+{
+    using System;
+
+    public class Person
+    {
+        const int MIN_NAME_LENGTH = 3;
+        const decimal MIN_SALARY = 460;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get => this.firstName;
+            private set
+            {
+                if (value?.Length < MIN_NAME_LENGTH)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => this.lastName;
+            private set
+            {
+                if (value?.Length < MIN_NAME_LENGTH)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => this.age;
+            private set
+            {
+                if (age < 1)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get => this.salary;
+            private set
+            {
+                if (value < MIN_SALARY)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} gets {this.Salary:f2} leva.";
+        }
+    }
+}
