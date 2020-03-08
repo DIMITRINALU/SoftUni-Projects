5dd06b17c1e82ece3585ef1d423fc96a152da060
dimitrinalu <lyubchova.dimitrina@gmail.com>
2020-03-08 17:53:57 +0200

    AddChanges

--- C#Advanced/10. BasicAlgorithms/P01. RecursiveArraySum/Program.cs
+++ C#Advanced/10. BasicAlgorithms/P01. RecursiveArraySum/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace P01.RecursiveArraySum
+﻿namespace P01.RecursiveArraySum
 {
+    using System;
+    using System.Linq;
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int index = 0;
+            int result = Sum(array, index);
+
+            Console.WriteLine(result);
+        }
+
+        private static int Sum(int[] array, int index)
+        {
+            if (index == array.Length - 1)
+            {
+                return array[index];
+            }
+
+            return array[index] + Sum(array, index + 1);
         }
     }
 }
